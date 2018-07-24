ea07ad1d3108124efd7122fc184783878a7336ca
Aniya <rachelle.payton30@gmail.com>
2018-07-24 15:45:50 -0500

    added javascript

---
+++ script1.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	
+	console.log("Heyyyy");
+	console.log("Coding is cool")
+	
+	$("button").click(function() {
+		console.log("Clicked");
+		$("body").css("background-color", " 
+	});	
+	
+	
+});
+
+var activity = $("#activity").val();
+		console.log("activity");
+	
+	
+	font-family: 'Dancing Script', cursive;
---
+++ script2.js
@@ -0,0 +1,24 @@
+$(document).ready(function() { 
+
+		console.log("heyyy");
+	
+	$("button").click( function() {
+		console.log("clicked");
+		//("body').css("background-color", "lightskyblue");
+		
+		
+		var name = $("#first_name").val();
+		console.log(name);
+		
+		
+		
+	});
+	
+	
+	
+	
+	
+	
+
+	});
+	
